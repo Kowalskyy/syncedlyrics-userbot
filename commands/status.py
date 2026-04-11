045abb91ac646e83b22f40dcf7f972b7544edf35
krahe <bingchilling@pissmail.com>
2026-04-12 01:39:47 +0300

    made spotify-like hypertext links

--- commands/status.py
+++ commands/status.py
@@ -1,5 +1,5 @@
 from config import CACHE_CHANNEL, DEFAULT_LINE, DEFAULT_COVER
-from discord import Activity, ActivityType, ActivityButton
+from discord import Activity, ActivityType
 from discord.ext import commands, tasks
 from utils import get_media_info
 from logging import getLogger
@@ -19,15 +19,14 @@ class Status(commands.Cog):
 		self.current_lyrics = []
 		self.last_sent_line = ""
 
-		self.proxied_cover_url = None
-
 		self.track_provider = None
-		self.track_url = None
 		self.track_duration = 0
 
 		self.last_reported_pos = -1
 		self.elapsed = 0.0
 		self.last_tick = time.time()
+
+		self.urls = {}
 
 	async def _fetch_lyrics(self, query: str):
 		raw = await asyncio.to_thread(syncedlyrics.search, query, False, True, None, ['Musixmatch', 'Lrclib', 'NetEase', 'Megalobiz'])
@@ -56,7 +55,9 @@ class Status(commands.Cog):
 					'params': {'term': query, 'entity': 'song', 'limit': 1, 'country': 'us'},
 					'cover_path': ['results', 0, 'artworkUrl100'], 
 					'duration_path': ['results', 0, 'trackTimeMillis'],
-					'url_path': ['results', 0, 'trackViewUrl'],
+					'track_url_path': ['results', 0, 'trackViewUrl'],
+					'artist_url_path': ['results', 0, 'artistViewUrl'],
+					'album_url_path': ['results', 0, 'collectionViewUrl'],
 					'result_key': 'resultCount'
 				},
 				'deezer': {
@@ -64,24 +65,32 @@ class Status(commands.Cog):
 					'params': {'q': query, 'limit': 1},
 					'cover_path': ['data', 0, 'album', 'cover_xl'], 
 					'duration_path': ['data', 0, 'duration'],
-					'url_path': ['data', 0, 'link'],
+					'track_url_path': ['data', 0, 'link'],
+					'artist_url_path': ['data', 0, 'artist', 'link'],
+					'album_url_path': ['data', 0, 'album', 'id'],
 					'result_key': 'total'
 				}
 			}
 			try:
 				for name, service in services.items():
-					base_url, params, cpath, dpath, upath, rkey = service.values()
+					base_url, params, coverp, durationp, trackp, artistp, albump, rkey = service.values()
 					rjson = (await client.get(base_url, params=params)).json()
 					if rjson.get(rkey, 0) > 0:
 						self.track_provider = name
 						def walk(data, path):
 							for k in path: data = data[k] if isinstance(data, list) else data.get(k, {})
 							return data
-						cover_url, self.track_duration, self.track_url = walk(rjson, cpath), walk(rjson, dpath), walk(rjson, upath)
+						cover_url, self.track_duration = walk(rjson, coverp), walk(rjson, durationp)
+						self.urls = {
+							'track': walk(rjson, trackp),
+							'artist': walk(rjson, artistp),
+							'album': walk(rjson, albump)
+						}
 						if name == 'apple.music': 
 							cover_url = cover_url.replace('100x100', '1000x1000')
 							self.track_duration /= 1000
-							self.track_url = self.track_url.split('?')[0]
+							self.urls['track'] = self.urls['track'].split('?')[0]
+						if name == 'deezer': self.urls['album'] = f"https://www.deezer.com/album/{self.urls['album']}"
 						if cover_url: logger.info(f'found info on {name}'); break
 			except Exception as e: logger.error(f'cover not found due to {e}')
 
@@ -96,7 +105,7 @@ class Status(commands.Cog):
 				if msg.embeds and (img := (msg.embeds[0].thumbnail or msg.embeds[0].image)):
 					if img.proxy_url and '/external/' in img.proxy_url:
 						if self.current_track == expected_track:
-							self.proxied_cover_url = f"mp:external/{img.proxy_url.split('/external/')[1]}"
+							self.urls['cover'] = f"mp:external/{img.proxy_url.split('/external/')[1]}"
 							self.last_sent_line = ""
 						break
 			await msg.delete()
@@ -131,13 +140,12 @@ class Status(commands.Cog):
 			self.current_track = full_name
 			self.current_lyrics = []
 			self.last_sent_line = ""
-			self.proxied_cover_url = None
 			self.track_provider = None
-			self.track_url = None
 			self.track_duration = 0
 			self.last_reported_pos = -1
 			self.elapsed = 0.0
 			self.last_tick = time.time()
+			self.urls = {}
 
 			asyncio.create_task(self._fetch_lyrics(full_name))
 			asyncio.create_task(self._fetch_and_proxy_cover(artist, title, full_name))
@@ -181,13 +189,15 @@ class Status(commands.Cog):
 				application_id=1295028329069674609,
 				name = f'{title} by {artist}'[:128],
 				details = f'🎤 {current_line}'[:128],
+				details_url = self.urls.get('album', '') if self.urls.get('album', '') else None,
 				state=title[:128],
+				state_url=self.urls.get('track', '') if self.urls.get('track', '') else None,
 				timestamps=ts_payload, #type: ignore it's fine as long discord doesn't change shit
 				assets = {
-					'large_image': self.proxied_cover_url if self.proxied_cover_url else f'mp:external/{DEFAULT_COVER}',
-					'large_text': f'by {artist}'[:128]
-				},
-				buttons = [ActivityButton(f'listen on {self.track_provider}', self.track_url)] if self.track_provider and self.track_url else [] 
+					'large_image': self.urls.get('cover', '') if self.urls.get('cover', '') else f'mp:external/{DEFAULT_COVER}',
+					'large_text': f'by {artist}'[:128],
+					'large_url': self.urls.get('artist', '') if self.urls.get('artist', '') else ''
+				}
 			)
 			await self.bot.change_presence(activity=activity)
 
